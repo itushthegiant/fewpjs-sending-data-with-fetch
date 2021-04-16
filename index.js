68f136fcfe27bed65e08fd577b74d8d0c0003cf4
etamar <etamar.levy@gmail.com>
2021-04-15 21:40:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', configurationObject)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
+
+
+
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
