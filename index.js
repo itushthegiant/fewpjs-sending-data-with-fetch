9240df891c60a5b238ffa52aa69f65a7aaddd2ea
etamar <etamar.levy@gmail.com>
2021-04-15 22:21:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 // Add your code here
 function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
     fetch('http://localhost:3000/users', configurationObject)
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => {
+        let id = data["id"]
+        const body = document.querySelector('body')
+        body.innerHTML = id;
+    })
+    
+    .catch(function(error) {
+      alert('Unauthorized Access');
+      console.log(error.message);
+    }); 
 }
+ submitData("etamar", "gadshgdfv")
 
-
-
-let formData = {
-    name: "Steve",
-    email: "steve@steve.com"
-  };
-
-  const configurationObject = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  };
